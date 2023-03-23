95698da22bf24fa3398d6e8168c222eb1c8b8adc
ThomasMattern2 <113630601+ThomasMattern2@users.noreply.github.com>
2023-03-23 10:27:58 -0600

    setup

--- TestDesign.java
+++ TestDesign.java
@@ -1,5 +1,21 @@
 package edu.ucalgary.oop;
 
-public class TestDesign {
+import org.junit.*;
+import org.junit.Before;
+import static org.junit.Assert.*;
 
-}
+
+public class TestDesign {
+    private Animal animal1;
+    private Treatment treatment1;
+    private Schedule schedule1;
+
+    /**
+     * Set up instances of Animal and Treatment for testing purposes.
+     */
+    @Before
+    public void setUp() {
+        animal1 = new Animal(1, "Dog", "Scout", DIURNAL, careNeeded, feedingSchedule, 12);
+        treatment1 = new Treatment(2, 12);
+        schedule1 = new Schedule(animals[]);
+    }
