2c2e8d1eba2d68bb0e853b38af0cf8af241d8293
ThomasMattern2 <113630601+ThomasMattern2@users.noreply.github.com>
2023-03-23 11:49:42 -0600

    animal implementation

--- TestDesign.java
+++ TestDesign.java
@@ -1,21 +1,49 @@
-package edu.ucalgary.oop;
-
-import org.junit.*;
-import org.junit.Before;
-import static org.junit.Assert.*;
-
-
-public class TestDesign {
-    private Animal animal1;
-    private Treatment treatment1;
-    private Schedule schedule1;
-
-    /**
-     * Set up instances of Animal and Treatment for testing purposes.
-     */
-    @Before
+   @Before
     public void setUp() {
         animal1 = new Animal(1, "Dog", "Scout", DIURNAL, careNeeded, feedingSchedule, 12);
         treatment1 = new Treatment(2, 12);
         schedule1 = new Schedule(animals[]);
+    }
+
+    // Animal tests
+    @Test
+    public void testGetAnimalId() {
+        int id = animal1.getId();
+        assertEquals("Animal ID should be 1, but it's not", 1, id);
+    }
+
+    @Test
+    public void testGetType() {
+        String type = animal1.getType();
+        assertEquals("Animal type should be 'Dog', but it's not", "Dog", type);
+    }
+
+    @Test
+    public void testGetName() {
+        String name = animal1.getName();
+        assertEquals("Animal name should be 'Scout', but it's not", "Scout", name);
+    }
+
+    @Test
+    public void testGetActivityPattern() {
+        ActivityPattern activityPattern = animal1.getActivityPattern();
+        assertEquals("Animal activity pattern should be DIURNAL, but it's not", ActivityPattern.DIURNAL, activityPattern);
+    }
+
+    @Test
+    public void testGetCareNeeded() {
+        String careNeeded = animal1.getCareNeeded();
+        assertEquals("Animal care needed should be 'careNeeded', but it's not", "careNeeded", careNeeded);
+    }
+
+    @Test
+    public void testGetFeedingSchedule() {
+        String feedingSchedule = animal1.getFeedingSchedule();
+        assertEquals("Animal feeding schedule should be 'feedingSchedule', but it's not", "feedingSchedule", feedingSchedule);
+    }
+
+    @Test
+    public void testGetAge() {
+        int age = animal1.getAge();
+        assertEquals("Animal age should be 12, but it's not", 12, age);
     }
