5fe2b31e72873c74bec8bfd6320bef979dc31de6
Thomas Mattern <113630601+ThomasMattern2@users.noreply.github.com>
2023-04-03 16:40:35 -0600

    cleaned up working task

--- edu/ucalgary/oop/Main.java
+++ edu/ucalgary/oop/Main.java
@@ -1,92 +1,106 @@
 package edu.ucalgary.oop;
 
 import java.sql.*;
-import java.util.ArrayList;
+import java.util.*;
+
 
 public class Main {
-    private static ResultSet results;
-    public static void createConnection() {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/EWR", "root", "Root");
-        } catch (ClassNotFoundException | SQLException e) {
-            e.printStackTrace();
-        }
-    }
-    
-    public static ArrayList<Treatment> getAnimalTreatment(int animalID) {
-        ArrayList<Treatment> animalTreatments = new ArrayList<>();
-        try {
-            Connection connector = DriverManager.getConnection("jdbc:mysql://localhost/EWR", "root", "Root");
-            Statement stmt = connector.createStatement();
-            ResultSet results = stmt.executeQuery("SELECT TREATMENTS.TaskID, TREATMENTS.StartHour, TASKS.Description, TASKS.Duration, TASKS.MaxWindow FROM TREATMENTS INNER JOIN TASKS ON TREATMENTS.TaskID = TASKS.TaskID WHERE TREATMENTS.AnimalID = " + animalID);
-
-            while (results.next()) {
-                int taskID = results.getInt("TaskID");
-                int startTime = results.getInt("StartHour");
-                String description = results.getString("Description");
-                int duration = results.getInt("Duration");
-                int timeWindow = results.getInt("MaxWindow");
-
-                Task task = new Task(taskID, duration, timeWindow, description);
-                Treatment treatment = new Treatment(taskID, startTime, task);
-                animalTreatments.add(treatment);
-            }
-
-            connector.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        return animalTreatments;
-    }
     public static void main(String[] args) {
-        createConnection();
         try {
             Connection connector = DriverManager.getConnection("jdbc:mysql://localhost/EWR", "root", "Root");
             Statement stmt = connector.createStatement();
             ResultSet results = stmt.executeQuery("SELECT * FROM ANIMALS");
+
             ArrayList<Animal> animals = new ArrayList<>();
+            Map<Integer, Animal> animalMap = new HashMap<>();
 
             while (results.next()) {
                 int animalID = results.getInt("AnimalID");
                 String nickname = results.getString("AnimalNickname");
                 String species = results.getString("AnimalSpecies");
                 ActiveHours activeHours;
-                ArrayList<Treatment> careNeeded = getAnimalTreatment(animalID);
+                ArrayList<Treatment> careNeeded = new ArrayList<>();
                 int timeToFeed = 0;
+
+                Animal animal = null;
 
                 switch (species) {
                     case "coyote":
-                        Coyote coyote = new Coyote(animalID, nickname, careNeeded);
-                        animals.add(coyote);
-                        System.out.println("Coyote ID: " + coyote.getAnimalID() + ", Nickname: " + coyote.getName() + ", Care Needed: " + coyote.getCareNeeded());
+                        activeHours = ActiveHours.DIURNAL;
+                        animal = new Coyote(animalID, nickname, careNeeded);
                         break;
                     case "fox":
-                        Fox fox = new Fox(animalID, nickname, careNeeded);
-                        animals.add(fox);
+                        activeHours = ActiveHours.DIURNAL;
+                        animal = new Fox(animalID, nickname, careNeeded);
                         break;
                     case "porcupine":
-                        Porcupine porcupine = new Porcupine(animalID, nickname, careNeeded);
-                        animals.add(porcupine);
+                        activeHours = ActiveHours.DIURNAL;
+                        animal = new Porcupine(animalID, nickname, careNeeded);
                         break;
                     case "beaver":
-                        Beaver beaver = new Beaver(animalID, nickname, careNeeded);
-                        animals.add(beaver);
+                        activeHours = ActiveHours.DIURNAL;
+                        animal = new Beaver(animalID, nickname, careNeeded);
                         break;
                     case "raccoon":
-                        Raccoon raccoon = new Raccoon(animalID, nickname, careNeeded);
-                        animals.add(raccoon);
-
+                        activeHours = ActiveHours.DIURNAL;
+                        animal = new Raccoon(animalID, nickname, careNeeded);
                         break;
                     default:
-                    	// to set activeHours to something
-                    	activeHours = ActiveHours.DIURNAL;
-                    	Orphan orphan = new Orphan(animalID, nickname, activeHours, careNeeded, timeToFeed, timeToFeed, timeToFeed);
-                    	animals.add(orphan);
+                        // to set activeHours to something
+                        activeHours = ActiveHours.DIURNAL;
+                        animal = new Orphan(animalID, nickname, activeHours, careNeeded, timeToFeed, timeToFeed, timeToFeed);
+                        System.out.println("Unknown animal species: " + species);
+                }
+
+                if (animal != null) {
+                    animals.add(animal);
+                    animalMap.put(animalID, animal);
                 }
             }
+            ArrayList<Task> tasks = new ArrayList<>();
+            Map<Integer, Task> taskMap = new HashMap<>();
+            ResultSet taskResults = stmt.executeQuery("SELECT * FROM TASKS");
 
+            while (taskResults.next()) {
+                int taskID = taskResults.getInt("TaskID");
+                int duration = taskResults.getInt("Duration");
+                int maxWindow = taskResults.getInt("MaxWindow");
+                String description = taskResults.getString("Description");
+
+                Task task = new Task(taskID, duration, maxWindow, description);
+                tasks.add(task);
+                taskMap.put(task.getID(), task);        
+           }
+            ArrayList<Treatment> treatments = new ArrayList<>();
+            ResultSet treatmentResults = stmt.executeQuery("SELECT * FROM TREATMENTS");
+
+            while (treatmentResults.next()) {
+                int animalID = treatmentResults.getInt("AnimalID");
+                int taskID = treatmentResults.getInt("TaskID");
+                int startHour = treatmentResults.getInt("StartHour");
+
+                if (animalMap.containsKey(animalID) && taskMap.containsKey(taskID)) {
+                    Treatment treatment = new Treatment(taskID, startHour);
+                    treatments.add(treatment);
+
+                    // Add the treatment to the corresponding animal's careNeeded list
+                    Animal animal = animalMap.get(animalID);
+                    animal.getCareNeeded().add(treatment);
+                } else {
+                    System.out.println("Invalid AnimalID or TaskID: " + animalID + ", " + taskID);
+                }
+            }
+            for (Animal animal : animals) {
+                System.out.println("Animal ID: " + animal.getAnimalID() + " | Animal Nickname: " + animal.getName());
+                System.out.println("Care Needed:");
+
+                ArrayList<Treatment> treatments1 = animal.getCareNeeded();
+                for (Treatment treatment2 : treatments1) {
+                	System.out.println(treatment2.getTaskID());
+                }
+
+                System.out.println();
+            }
             connector.close();
         } catch (SQLException e) {
             e.printStackTrace();
