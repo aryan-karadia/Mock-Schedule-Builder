1002c6f750d5eca6e15cf389257f9d3b5964e759
Aryan Karadia <105018373+aryan-karadia@users.noreply.github.com>
2023-04-03 14:48:01 -0600

    Changed Orphan constructor

--- edu/ucalgary/oop/Orphan.java
+++ edu/ucalgary/oop/Orphan.java
@@ -2,8 +2,12 @@ package edu.ucalgary.oop;
 import java.util.ArrayList;
 
 public class Orphan extends Animal {
-    public Orphan(int animalID, String name, ActiveHours activeHours, ArrayList<Treatment> careNeeded, int timeToFeed) {
-        super(animalID, "Orphan", name, activeHours, careNeeded, timeToFeed);
-    }
+    public Orphan(int animalID, String name, ActiveHours activeHours, ArrayList<Treatment> careNeeded, int timeToFeed, int timeToPrepFood, int timeToClean) {
+        super(animalID, "Orphan", name, activeHours, careNeeded, timeToFeed, timeToPrepFood, timeToClean);
+        }
+        @Override
+    public boolean isFed() {
+        return true;
+        }
 }
 
