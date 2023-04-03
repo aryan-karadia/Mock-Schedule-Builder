5e46b8cc0c047c5d8b541aa045f31499993fc2b2
AdityaP16 <ap.prasad@outlook.com>
2023-04-03 17:29:23 -0600

    schedule fix

--- edu/ucalgary/oop/Schedule.java
+++ edu/ucalgary/oop/Schedule.java
@@ -1,28 +1,54 @@
 package edu.ucalgary.oop;
-import java.awt.event.ActionListener;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
-import java.util.concurrent.ScheduledFuture;
-
-
-public class Schedule extends JFrame implements ActionListener{
-
-    private static HashMap<Integer, String> workingHours = new HashMap<>(24);
+public class Schedule {
     private boolean backupVolunteerNeeded = false;
     private ArrayList<Animal> animals;
-    private HashMap<Integer, ArrayList<Task>> tasks = new HashMap<>(24);
+    private HashMap<Integer, ArrayList<Treatment>> tasks = new HashMap<>(24);
 
-    public Schedule(Animal[] animals) {
-        this.animals = new ArrayList<>(Arrays.asList(animals));
-    }
+    private HashMap<Integer, Integer> availableMinutes = new HashMap<>(24);
 
-    public Schedule() {
-        // todo
+    // Creates schedule object and schedules mandatory medical tasks
+    // @param animals - ArrayList of all animals in the rescue center
+    public Schedule(ArrayList<Animal> animals) throws IllegalArgumentException {
+        generateDefaultSchedule();
+        this.animals = new ArrayList<>(animals);
+        // for every animal, find what time the tasks are required to be done at
+        for (Animal animal: animals) {
+            for (Treatment treatment : animal.getCareNeeded()) {
+                // Checks if time is available
+                int startTime = treatment.getStartTime();
+                // if time is taken and available minutes is 0
+                if (tasks.get(startTime) != null && availableMinutes.get(startTime) == 0) {
+                    // todo - GUI element which tells person to move a certain task
+                }
+                // if time is taken but available minute is not 0, need to call backup volunteer
+                else if (tasks.get(startTime) != null) {
+                    setBackupNeeded(true);
+
+
+                }
+                // else time is available
+                else {
+                    // update available minutes for given hour
+                    if (availableMinutes.get(startTime) - treatment.getTask().getDURATION() < 0) {
+                        // Not enough time left in the hour to do treatment
+                        // todo - GUI element to tell person to move one of the tasks
+                    }
+                    else {
+                        // puts treatment into ArrayList of treatments which correspond to that hour
+                        tasks.get(startTime).add(treatment);
+                        availableMinutes.put(startTime, availableMinutes.get(startTime) - treatment.getTask().getDURATION());
+                    }
+                }
+            }
+        }
+        // schedules feeding
+        scheduleFeedingAndCleaningTasks();
     }
 
     public boolean getBackupNeeded() {
@@ -33,8 +59,17 @@ public class Schedule extends JFrame implements ActionListener{
         this.backupVolunteerNeeded = needed;
     }
 
+    // Finds all available time and schedules feeding and cleaning
     public void scheduleFeedingAndCleaningTasks() {
-        //  todo
+        while (true) {
+            ArrayList<Integer> availabaleHours = new ArrayList<>(24);
+            for (Map.Entry<Integer, Integer> entry : this.availableMinutes.entrySet()) {
+                if (entry.getValue() != 0) {
+                    availabaleHours.add(entry.getKey());
+                }
+
+            }
+        }
     }
 
     public String getFormattedSchedule() {
@@ -43,39 +78,37 @@ public class Schedule extends JFrame implements ActionListener{
     }
 
     public void addTask(int hour, Task task) {
-    	//todo
+        //todo
     }
 
     public void removeTask(int hour, Task task) {
-    	//todo
+        //todo
     }
 
-    public void actionPerformed(ActionEvent event){
-        JOptionPane.showMessageDialog(null, "This Button has been pushed.");
+    public ArrayList<Animal> getAnimals() {
+        return this.animals;
     }
 
+    /** Helper functions **/
+
+    // Sets all values for workingHours to null for every hour and available minutes in each hour as 60
+    private void generateDefaultSchedule() {
+        for (int i = 0; i < 24; i++) {
+            tasks.put(i, null);
+            availableMinutes.put(i, 60);
+        }
+    }
     public static void main(String[] args) {
+        ArrayList<Treatment> lonerTreatments = new ArrayList<>();
+        Task eyedrops = new Task(9, 25, 1, "Eyedrops");
+        lonerTreatments.add(new Treatment(9, 22, eyedrops));
 
-            EventQueue.invokeLater(() -> {
-                //Creating the frame, setting the dimensions, and exiting on close.
-                JFrame frame = new JFrame();
-                frame.setSize(400,400);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Coyote loner = new Coyote(1, "loner", lonerTreatments);
+        System.out.println(loner.getClass());
+        ArrayList<Animal> animals = new ArrayList<>();
+        animals.add(loner);
 
-                //Creating a panel first before adding the button into the panel.
-                JPanel buttonsPanel = new JPanel();
-                JButton button1 = new JButton("Click Here!");
-
-                //Create a button listener,
-                Schedule buttonListener = new Schedule();
-                button1.addActionListener(buttonListener);
-
-                //adding the button onto the North side of the panel.
-                buttonsPanel.add(button1);
-                frame.getContentPane().add(BorderLayout.NORTH, buttonsPanel);
-                //turn the frame on
-                frame.setVisible(true);
-
-            });
+        Schedule testSchedule = new Schedule(animals);
+        System.out.println(testSchedule.getAnimals());
     }
 }
