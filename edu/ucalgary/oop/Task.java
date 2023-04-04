079a159bafb8102349764702d18e7f73d50ae579
darkicewolf50 <brock.tomlinson@ucalgary.ca>
2023-04-04 14:38:12 -0600

    changed comments so that they probabily work with javadoc

--- edu/ucalgary/oop/Task.java
+++ edu/ucalgary/oop/Task.java
@@ -1,40 +1,45 @@
-/*
-This code creates a task class that has 4 variables.
-The code starts by defining a package.
-The code then defines a "Task" class.
-The "Task" class contains 4 private variables and a constructor.
-The constructor takes 4 parameters.
-The constructor sets the 4 private variables to the 4 parameters.
-The "Task" class also contains 4 getters that return the values of the 4 private variables.
-*/
+/**
+ * This code creates a task class that has 4 variables.
+ * The code starts by defining a package.
+ * The code then defines a "Task" class.
+ * The "Task" class contains 4 private variables and a constructor.
+ * The constructor takes 4 parameters.
+ * The constructor sets the 4 private variables to the 4 parameters.
+ * The "Task" class also contains 4 getters that return the values of the 4 private variables.
+ */
 package edu.ucalgary.oop;
 
 public class Task {
+    
+    /**
+     * The unique ID of the task.
+     */
     private final int TASKID;
+    
+    /**
+     * The duration of the task in minutes.
+     */
     private final int DURATION;
+    
+    /**
+     * The time window during which the task can be completed, in minutes.
+     */
     private final int timeWindow;
+    
+    /**
+     * A brief description of the task.
+     */
     private final String description;
 
+    /**
+     * Constructor for the Task class.
+     * 
+     * @param taskID      the unique ID of the task
+     * @param duration    the duration of the task in minutes
+     * @param timeWindow  the time window during which the task can be completed, in minutes
+     * @param description a brief description of the task
+     */
     public Task(int taskID, int duration, int timeWindow, String description) {
-    /*
-    Constructor for Task class.
-
-    Parameters
-    ----------
-    taskID: int
-        The task ID.
-    duration: int
-        The duration of the task.
-    timeWindow: int
-        The time window of the task.
-    description: String
-        The description of the task.
-
-    Returns
-    -------
-    None
-
-    */
         // todo - validate input
         this.TASKID = taskID;
         this.DURATION = duration;
@@ -42,71 +47,39 @@ public class Task {
         this.description = description;
     }
 
+    /**
+     * Returns the unique ID of the task.
+     * 
+     * @return the task ID
+     */
     public int getID() {
-    /*
-    Gets the ID of the task.
-
-    Parameters
-    ----------
-    None
-
-    Returns
-    -------
-    int
-        The task ID.
-
-    */
         return this.TASKID;
     }
 
+    /**
+     * Returns the duration of the task in minutes.
+     * 
+     * @return the task duration
+     */
     public int getDURATION() {
-    /*
-    Gets the duration of the task.
-
-    Parameters
-    ----------
-    None
-
-    Returns
-    -------
-    int
-        The duration of the task.
-
-    */
         return this.DURATION;
     }
 
+    /**
+     * Returns the time window during which the task can be completed, in minutes.
+     * 
+     * @return the task time window
+     */
     public int getTimeWindow() {
-    /*
-    Gets the time window of the task.
-
-    Parameters
-    ----------
-    None
-
-    Returns
-    -------
-    int
-        The time window of the task.
-
-    */
         return this.timeWindow;
     }
 
+    /**
+     * Returns a brief description of the task.
+     * 
+     * @return the task description
+     */
     public String getDescription() {
-    /*
-    Gets the description of the task.
-
-    Parameters
-    ----------
-    None
-
-    Returns
-    -------
-    String
-        The description of the task.
-
-    */
         return this.description;
     }
 }
