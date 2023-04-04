df63025e7dd5891411b8e2e8bc9752fc6eb6dd21
darkicewolf50 <brock.tomlinson@ucalgary.ca>
2023-04-04 16:07:43 -0600

    added javadoc comments to animal.java, treatment.java and active hours

--- edu/ucalgary/oop/ActiveHours.java
+++ edu/ucalgary/oop/ActiveHours.java
@@ -1,34 +1,45 @@
-package edu.ucalgary.oop;
-import java.util.ArrayList;
+/**
+ * This code defines the ActiveHours enum and has 3 constants, NOCTURNAL, DIURNAL, and CREPUSCULAR.
+ * The code starts by importing the "ArrayList" class from java.util.
+ * The code then defines the "ActiveHours" enum and has 3 constants, NOCTURNAL, DIURNAL, and CREPUSCULAR.
+ * The "NOCTURNAL" constant has a method named "feedingHours" that returns an ArrayList that contains the numbers 0, 1, and 2.
+ * The "DIURNAL" constant has a method named "feedingHours" that returns an ArrayList that contains the numbers 8, 9, and 10.
+ * The "CREPUSCULAR" constant has a method named "feedingHours" that returns an ArrayList that contains the numbers 19, 20 and 21.
+ */
 
-public enum ActiveHours {
-	NOCTURNAL {
-        public ArrayList<Integer> feedingHours() {
-            ArrayList<Integer> hours = new ArrayList<>();
-            hours.add(0);
-            hours.add(1);
-            hours.add(2);
-            return hours;
-        }
-    },
-    DIURNAL {
-        public ArrayList<Integer> feedingHours() {
-            ArrayList<Integer> hours = new ArrayList<>();
-            hours.add(8);
-            hours.add(9);
-            hours.add(10);
-            return hours;
-        }
-    },
-    CREPUSCULAR {
-        public ArrayList<Integer> feedingHours() {
-            ArrayList<Integer> hours = new ArrayList<>();
-            hours.add(19);
-            hours.add(20);
-            hours.add(21);
-            return hours;
-        }
-    };
+ package edu.ucalgary.oop;
 
-    public abstract ArrayList<Integer> feedingHours();
-}
+ import java.util.ArrayList;
+ 
+ public enum ActiveHours {
+     NOCTURNAL {
+         public ArrayList<Integer> feedingHours() {
+             ArrayList<Integer> hours = new ArrayList<>();
+             hours.add(0);
+             hours.add(1);
+             hours.add(2);
+             return hours;
+         }
+     },
+     DIURNAL {
+         public ArrayList<Integer> feedingHours() {
+             ArrayList<Integer> hours = new ArrayList<>();
+             hours.add(8);
+             hours.add(9);
+             hours.add(10);
+             return hours;
+         }
+     },
+     CREPUSCULAR {
+         public ArrayList<Integer> feedingHours() {
+             ArrayList<Integer> hours = new ArrayList<>();
+             hours.add(19);
+             hours.add(20);
+             hours.add(21);
+             return hours;
+         }
+     };
+ 
+     public abstract ArrayList<Integer> feedingHours();
+ }
+ 
--- edu/ucalgary/oop/Treatment.java
+++ edu/ucalgary/oop/Treatment.java
@@ -1,27 +1,72 @@
+/**
+ * The Treatment class represents a treatment that consists of a task ID, a start 
+ * time, and a task to be performed.
+ */
 package edu.ucalgary.oop;
 
 public class Treatment {
+    
+    /**
+     * The ID of the task to be performed.
+     */
     private final int TASKID;
+    
+    /**
+     * The start time of the treatment.
+     */
     private final int STARTTIME;
+    
+    /**
+     * The task to be performed.
+     */
     private Task task;
     
+    /**
+     * Constructor for the Treatment class.
+     * 
+     * @param taskID    the ID of the task to be performed
+     * @param startTime the start time of the treatment
+     * @param task      the task to be performed
+     */
     public Treatment(int taskID, int startTime, Task task) {
         // todo - validate input
         this.TASKID = taskID;
         this.STARTTIME = startTime;
         this.task = task;
     }
+    
+    /**
+     * Sets the task to be performed.
+     * 
+     * @param task the task to be performed
+     */
     public void setTask(Task task) {
         this.task = task;
     }
+    
+    /**
+     * Returns the task to be performed.
+     * 
+     * @return the task to be performed
+     */
     public Task getTask() {
         return this.task;
     }
     
+    /**
+     * Returns the start time of the treatment.
+     * 
+     * @return the start time of the treatment
+     */
     public int getStartTime() {
         return this.STARTTIME;
     }
 
+    /**
+     * Returns the ID of the task to be performed.
+     * 
+     * @return the ID of the task to be performed
+     */
     public int getTaskID() {
         return this.TASKID;
     }
