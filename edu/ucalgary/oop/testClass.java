df7ceeb8d36df09df6a39fbeaa27f0cff777efd2
Thomas Mattern <113630601+ThomasMattern2@users.noreply.github.com>
2023-04-04 15:30:36 -0600

    some new logic tests

--- edu/ucalgary/oop/testClass.java
+++ edu/ucalgary/oop/testClass.java
@@ -39,5 +39,40 @@ public class testClass {
         assertEquals(task1.getTimeWindow(), treatment1.getTask().getTimeWindow());
         assertEquals(task1.getDescription(), treatment1.getTask().getDescription());
     }
+    // Test if the animal object has the correct careNeeded list assigned
+    @Test
+    public void testAnimalCareNeededAssignment() {
+        ArrayList<Treatment> careNeeded = new ArrayList<>();
+        Coyote animal1 = new Coyote(1, "Jared", careNeeded);
+        assertEquals(careNeeded, animal1.getCareNeeded());
+    }
+    // Test if the animal object's careNeeded list contains the expected Treatment object
+    @Test
+    public void testAnimalCareNeededContents() {
+        Task task1 = new Task(1, 5, 3, "Grooming");
+        Treatment treatment1 = new Treatment(1, 3, task1);
+        ArrayList<Treatment> careNeeded = new ArrayList<>();
+        careNeeded.add(treatment1);
+        Coyote animal1 = new Coyote(1, "Jared", careNeeded);
+        assertEquals(1, animal1.getCareNeeded().size());
+        assertSame(treatment1, animal1.getCareNeeded().get(0));
+    }
+    // Test if the enum object has the correct feeding hours 
+    @Test
+    public void testEnumClass() {
+        ArrayList<Treatment> careNeeded = new ArrayList<>();
+        Coyote animal1 = new Coyote(1, "Jared", careNeeded);
+
+        // Test if the Coyote object has the correct ActiveHours
+        assertEquals(ActiveHours.CREPUSCULAR, animal1.getActiveHours());
+
+        // Test if the Coyote object has the correct feeding hours
+        ArrayList<Integer> expectedFeedingHours = new ArrayList<>();
+        expectedFeedingHours.add(19);
+        expectedFeedingHours.add(20);
+        expectedFeedingHours.add(21);
+
+        assertEquals(expectedFeedingHours, animal1.getActiveHours().feedingHours());
+    }
 
 }
